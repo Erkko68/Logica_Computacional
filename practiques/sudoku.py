307fb37bbc48318d9f984399ee887c245ccd1280
Èric Bitrià Ribes <47712373+Erkko68@users.noreply.github.com>
2022-12-01 20:17:26 +0100

    Added comments, optimized all the code in loops

--- practiques/sudoku.py
+++ practiques/sudoku.py
@@ -5,9 +5,12 @@ from sudoku_base import read_sudoku, var, visualize
 import sys
 from sys import exit
 
+#generate at least one (alo) clauses function
 
 def alo(lits):
     return [lits[:]]
+    
+#generate at most one (amo) clauses function
 
 def amo(lits):
     clauses = []
@@ -17,15 +20,17 @@ def amo(lits):
             lit2 = lits[j]
             clauses.append([~lit1,~lit2])
     return clauses
+
+#variation of the amo function but taking only the first clause of the list as a reference for pairwise combinations (function only used in miracle sudoku)
     
 def selfish_amo(lits):
     clauses = []
     for i in range(len(lits)):
         if i != 0:
-            lit1 = lits[0]
-            lit2 = lits[i]
-            clauses.append([~lit1,~lit2])
+            clauses.append([~lits[0],~lits[i]])
     return clauses
+
+#main function
 
 def solve(path):
     cnf = CNF()
@@ -50,7 +55,8 @@ def solve(path):
 
     # print('---------')
     
-    #values: Generate the amount of values needed in function of the size
+    #values: generate lists of required values ​​based on the size of the sudoku.
+    #the lists rows, columns and values are the same and could be used just one, but for readability i chose to do it this way.
     
     lst = []
     val = 0
@@ -61,7 +67,7 @@ def solve(path):
     columns = lst.copy()
     values = lst.copy()
     
-    # Cells: Create a set of clauses with each possible value
+    # Cells: Create a set of clauses with each possible value of each cell.
     
     for row in rows:
         lits = []
@@ -75,11 +81,12 @@ def solve(path):
             cnf.add_clauses(alo(lits))
             cnf.add_clauses(amo(lits))
             
-            #reset list to create next clause
+            #reset list
             
             lits = [] 
 
-    # Row
+    # Row: Create restrictions for each row.
+    
     for value in values:
         lits = []
         for row in rows:
@@ -91,11 +98,11 @@ def solve(path):
             
             cnf.add_clauses(amo(lits))
             
-            #reset list to create next clause
+            #reset list
             
             lits = [] 
 
-    # Column
+    # Column: Create restrictions for each column
     
     for value in values:
         lits = []
@@ -108,13 +115,13 @@ def solve(path):
             
             cnf.add_clauses(amo(lits))
             
-            #reset list to create next clause
+            #reset list
             
             lits = [] 
    
     # Subgroup
-    #Functionality: This function generates amo functions for each subgroup.
-    #It uses the variables i and j to define wich one of the subgroups clauses are being generated.
+    #This function generates amo functions for each subgroup.
+    #It uses the variables i and j to define wich one of the subgroup restriction clauses are being generated based on the subgroup lenght and height.
     
     for i in range(SUBGROUP_LENGTH):
     
@@ -135,202 +142,148 @@ def solve(path):
                 lits = []
     
     # Miracle Sudoku
+
+    '''
     
-    # Cells reachable with a knight move can not contain same value
-    #this code generates a set of clauses depending on each individual cell and creates a set of clauses to prevent the repetition of the origin cell value on the designed positions.
+    Each cell within horse movement distance of the origin cell cannot contain the same value.
+
+    Since I've optimized this section maybe too much not to repeat the code, it's a bit confusing, so I've decided to write a little explanation of what's going on here.
     
+    Let's consider the "X" as the possible positions of the knight and the "O" as the Knight:
+    
+    - - - - - - -
+    - - X - X - -
+    - X - - - X -
+    - - - O - - -
+    - X - - - X -
+    - - X - X - -
+    - - - - - - -
+    
+    What this code does is take the original "0" position and see if the other "X" positions are available on the board, to do this it uses the range of values ​​in i and j to determine the cell position, if the cell position cell is within the size of the board, it adds a new clause to the list. The order to determine each cell is like this:
+    
+    - - - - - - -
+    - - 1 - 2 - -
+    - 5 - - - 6 -
+    - - - O - - -
+    - 7 - - - 8 -
+    - - 3 - 4 - -
+    - - - - - - -
+    
+    Positions 1,2,3,4 correspond to the first if i,j and positions 5,6,7,8 to the second if j,i. I hope It helped a bit.
+    
+    '''
     for value in values:
         lits = []
         for row in rows:
             for column in columns:
+                for i in range(-2,3,4):
+                    for j in range(-1,2,2):
             
-            #Origin cell
+                        #Origin cell
             
-                lit = Bool('Cell_{}_{}_{}'.format(row,column,value))
-                lits.append(lit)
+                        lit = Bool('Cell_{}_{}_{}'.format(row,column,value))
+                        lits.append(lit)
             
-            #1st horse position
+                        #Knight position
             
-                if (int(row-2) >= 0) and (int(column-1) >= 0):
-                    lit = Bool('Cell_{}_{}_{}'.format(row-2,column-1,value))
-                    lits.append(lit)
+                        if (int(row+i) >= 0) and (int(column+j) >= 0) and (int(row+i) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)) and (int(column+j) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)):
+                        
+                            lit = Bool('Cell_{}_{}_{}'.format(row+i,column+j,value))
+                            lits.append(lit)         
+            
+                        if (int(row+j) >= 0) and (int(column+i) >= 0) and (int(row+j) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)) and (int(column+i) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)):
+                        
+                            lit = Bool('Cell_{}_{}_{}'.format(row+j,column+i,value))
+                            lits.append(lit)
                     
-            #2nd horse position    
-             
-                if (int(row-2) >= 0) and (int(column+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)):
-                    lit = Bool('Cell_{}_{}_{}'.format(row-2,column+1,value))
-                    lits.append(lit)
-                    
-            #3rd horse position
+                        #create clauses and reset list
             
-                if (int(row-1) >= 0) and (int(column-2) >= 0):
-                    lit = Bool('Cell_{}_{}_{}'.format(row-1,column-2,value))
-                    lits.append(lit)
-                  
-            #4rth horse position
-               
-                if (int(row-1) >= 0) and (int(column+2) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)):
-                    lit = Bool('Cell_{}_{}_{}'.format(row-1,column+2,value))
-                    lits.append(lit)
-                   
-            #5th horse position
-               
-                if (int(row+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)) and (int(column-2) >= 0):
-                    lit = Bool('Cell_{}_{}_{}'.format(row+1,column-2,value))
-                    lits.append(lit)
-            
-            #6th horse position
-               
-                if (int(row+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)) and (int(column+2) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)):
-                    lit = Bool('Cell_{}_{}_{}'.format(row+1,column+2,value))
-                    lits.append(lit)
-         
-            #7th horse position
-               
-                if (int(row+2) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)) and (int(column-1) >= 0):
-                    lit = Bool('Cell_{}_{}_{}'.format(row+2,column-1,value))
-                    lits.append(lit)
-         
-            #8th horse position
-               
-                if (int(row+2) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)) and (int(column+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)):
-                    lit = Bool('Cell_{}_{}_{}'.format(row+2,column+1,value))
-                    lits.append(lit)
-                    
-            #create clauses and reset list
-            
-                cnf.add_clauses(selfish_amo(lits))
-                lits = []  
+                        cnf.add_clauses(selfish_amo(lits))
+                        lits = []  
      
     # Cells reachable with a king move can not contain same value
-    #
+    
+    '''
+    - - - - - - -       - - - - - - -
+    - - - - - - -       - - - - - - -
+    - - X X X - -       - - 1 2 3 - -
+    - - X O X - -  -->  - - 4 O 5 - -
+    - - X X X - -       - - 6 7 8 - -
+    - - - - - - -       - - - - - - -
+    - - - - - - -       - - - - - - -
+    
+    '''
     
     for value in values:
         lits = []
         for row in rows:
             for column in columns:
+                for i in range(-1,2,2):
+                    for j in range(-1,2,2):
                 
-                #generate origin cell
+                        #generate origin cell
                 
-                lit = Bool('Cell_{}_{}_{}'.format(row,column,value))
-                lits.append(lit)
+                        lit = Bool('Cell_{}_{}_{}'.format(row,column,value))
+                        lits.append(lit)
                 
-                #Cells top
+                        #Cells
                 
-                if int(row-1) >= 0 and int(column-1) >=0:
+                        if ((int(row+i) >= 0) and (int(column+j) >=0)) and ((int(row+i) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT)) and (int(column+j) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT))):
                   
-                    lit = Bool('Cell_{}_{}_{}'.format(row-1,column-1,value))
-                    lits.append(lit)
-                    
-                if int(row-1) >= 0 and int(column) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                  
-                    lit = Bool('Cell_{}_{}_{}'.format(row-1,column,value))
-                    lits.append(lit)
+                            lit = Bool('Cell_{}_{}_{}'.format(row+i,column+j,value))
+                            lits.append(lit)
+ 
+                        #create clauses and reset list
+                
+                        cnf.add_clauses(selfish_amo(lits))
+                        lits = []  
 
-                if int(row-1) >= 0 and int(column+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                  
-                    lit = Bool('Cell_{}_{}_{}'.format(row-1,column+1,value))
-                    lits.append(lit)
-                
-                #Cells Mid
-                
-                if int(row) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(column-1) >= 0:
-                  
-                    lit = Bool('Cell_{}_{}_{}'.format(row,column-1,value))
-                    lits.append(lit)
-                
-                if int(row) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(column+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                  
-                    lit = Bool('Cell_{}_{}_{}'.format(row,column+1,value))
-                    lits.append(lit)
-                
-                #Cells bot
-                
-                if int(row+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(column-1) >= 0:
-                  
-                    lit = Bool('Cell_{}_{}_{}'.format(row+1,column-1,value))
-                    lits.append(lit)
-                    
-                if int(row+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(column) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                  
-                    lit = Bool('Cell_{}_{}_{}'.format(row+1,column,value))
-                    lits.append(lit)
-
-                if int(row+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(column+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                  
-                    lit = Bool('Cell_{}_{}_{}'.format(row+1,column+1,value))
-                    lits.append(lit)
-                    
-                #create clauses and reset list
-                
-                cnf.add_clauses(selfish_amo(lits))
-                lits = []  
-    
     # Cells reachable with a king move (without diagonal one) can not contain a consecutive value
+    
+    '''
+    - - - - - - -       - - - - - - -
+    - - - - - - -       - - - - - - -
+    - - - X - - -       - - - 1 - - -
+    - - X O X - -  -->  - - 3 O 4 - -
+    - - - X - - -       - - - 2 - - -
+    - - - - - - -       - - - - - - -
+    - - - - - - -       - - - - - - -
+    
+    '''
     
     for value in values:
         for row in rows:
             for column in columns:
+                for i in range(-1,2,2):
+                    for j in range(-1,2,2):
             
             	#generate origin cell
                 
-                lit = Bool('Cell_{}_{}_{}'.format(row,column,value))
-                lits.append(lit)
+                            lit = Bool('Cell_{}_{}_{}'.format(row,column,value))
+                            lits.append(lit)
                 
-                #top
-                
-                if int(row-1) >= 0 and int(value+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row-1,column,value+1))
-                    lits.append(lit)
+                            #top and bot
+                        
+                            if (int(row+i) >= 0 and int(value+j) >=0) and int(row+i) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(value+j) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
+                        
+                                lit = Bool('Cell_{}_{}_{}'.format(row+i,column,value+j))
+                                lits.append(lit)
+                            
                     
-                if int(row-1) >= 0 and int(value-1) >= 0:
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row-1,column,value-1))
-                    lits.append(lit)
+                            #left and right
+                        
+                            if (int(column+i) >= 0 and int(value+j) >=0) and int(column+i) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(value+j) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
+
+                                lit = Bool('Cell_{}_{}_{}'.format(row,column+i,value+j))
+                                lits.append(lit)
                 
-                #bot
+                            #create clauses and reset list
                 
-                if int(row+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(value+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row+1,column,value+1))
-                    lits.append(lit)
-                    
-                if int(row+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(value-1) >= 0:
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row+1,column,value-1))
-                    lits.append(lit)
-                    
-                #left
-                    
-                if int(column-1) >= 0 and int(value+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row,column-1,value+1))
-                    lits.append(lit)
-                    
-                if int(column-1) >= 0 and int(value-1) >= 0:
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row,column-1,value-1))
-                    lits.append(lit)
-                    
-                #right
-                
-                if int(column+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT) and int(value+1) < (SUBGROUP_LENGTH*SUBGROUP_HEIGHT):
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row,column+1,value+1))
-                    lits.append(lit)
-                
-                if int(column+1) >= 0 and int(value-1) >= 0:
-                                     
-                    lit = Bool('Cell_{}_{}_{}'.format(row,column+1,value-1))
-                    lits.append(lit)
-                
-                #create clauses and reset list
-                
-                cnf.add_clauses(selfish_amo(lits))
-                lits = [] 
+                            cnf.add_clauses(selfish_amo(lits))
+                            lits = [] 
     
-    #debug only
+    #debug only, count clauses
+    
     '''
     Clauses = 0
     for clause in cnf.clauses:
